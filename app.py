13eb7eedbd14837862c3ab95093e053ca9848010
Shreyans3700 <pandeyshreyansh46@gmail.com>
2026-04-09 00:50:49 +0530

    completed the prediction pipeline.

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 import os
 import sys
 import certifi
+import pandas as pd
 from dotenv import load_dotenv
 from network_security.exception.exception import NetworkSecurityException
 from network_security.logging.logger import logging
 from network_security.pipeline.training_pipeline import TrainingPipeline
 from network_security.constants.model_trainer import ModelTrainerArtifact
+from network_security.utils.utils import load_object
+from network_security.utils.ml_utils import NetworkModel
 
+from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, UploadFile, File
 from uvicorn import run as app_run
 from starlette.responses import RedirectResponse
 
 ca = certifi.where()
-
+templates = Jinja2Templates(directory="./templates")
 
 load_dotenv()
 
@@ -42,6 +46,34 @@ async def train_pipeline():
         train_pipeline = TrainingPipeline()
         train_artifact, model = train_pipeline.run_pipeline()
         logging.info(train_artifact.model_file_path)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+
+
+@app.post("/predict")
+async def predict_pipeline(request: Request, file: UploadFile = File(...)):
+    try:
+        df = pd.read_csv(file.file)
+
+        preprocessor = load_object("final_models/preprocessor.pkl")
+        model = load_object("final_models/model.pkl")
+
+        network_model = NetworkModel(preprocessor=preprocessor, model=model)
+
+        y_pred = network_model.predict(df)
+
+        print(y_pred)
+
+        df["predicted_column"] = y_pred
+
+        table_html = df.to_html(classes="table table-striped")
+
+        return templates.TemplateResponse(
+            request=request,
+            name="template.html",
+            context={"table": table_html}
+        )
+
     except Exception as e:
         raise NetworkSecurityException(e, sys)
 
